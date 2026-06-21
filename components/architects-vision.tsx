550fb95043153f7a049c7db0099379501eae78aa
De-Slorex1 <jacobsamson2000@gmail.com>
2026-06-21 18:19:54 +0100

    working

--- components/architects-vision.tsx
+++ components/architects-vision.tsx
@@ -10,7 +10,7 @@ const team = [
   {
     name: "Team Member",
     role: "CFO",
-    image: "/mojisola.png",
+    image: "/moji.png",
     bio: "Guiding learners through real-world software engineering and modern development practices.",
   },
   
